4f3f70b98c95c8526f51114d1222397f8424eb1d
Konrad Malawski <konrad.malawski@project13.pl>
2012-01-01 23:20:37 +0100

    initial commit

---
+++ src/main/java/pl/project13/ProtoDocMojo.java
@@ -0,0 +1,53 @@
+package pl.project13;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import pl.project13.protodoc.runner.ProtoDocMain;
+
+/**
+ * Goal which generates ProtoDoc.
+ *
+ * @author <a href="mailto:konrad.malawski@java.pl">Konrad 'ktoso' Malawski</a>
+ * @goal package
+ * @phase initialize
+ * @requiresProject
+ * @since 1.0
+ */
+@SuppressWarnings({"JavaDoc"})
+public class ProtoDocMojo extends AbstractMojo {
+    /**
+     * The maven project.
+     *
+     * @parameter expression="${project}"
+     * @readonly
+     */
+    //@VisibleForTesting
+    MavenProject project;
+
+
+    /**
+     * Where are the proto files located?
+     *
+     * @parameter default-value="${project.basedir}/src/main/proto"
+     */
+    private String protoDir;
+
+    /**
+     * Where are should the docs be generated to?
+     *
+     * @parameter default-value="${project.basedir}/target/protodoc"
+     */
+    private String outDir;
+
+    /**
+     * Should the plugin print a lot of debug info?
+     *
+     * @parameter default-value="false"
+     */
+    private boolean verbose;
+
+    public void execute() throws MojoExecutionException {
+        ProtoDocMain.generateProtoDoc(protoDir, outDir, verbose);
+    }
+}
